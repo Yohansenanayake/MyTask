82899b2a9f3c2a6fed9b0b53ffb47af3574df1d5
Yohan Senanayake <yohansenanayake4321@gmail.com>
2025-05-12 19:19:29 +0530

    config(firebase): Configured Firebase using CLI

--- todo_frontend/lib/main.dart
+++ todo_frontend/lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_frontend/features/authentication/ui/loginScreen.dart';
-import 'package:todo_frontend/features/authentication/ui/signUpScreen.dart';
-import 'package:todo_frontend/features/home/ui/homePage.dart';
+import 'package:todo_frontend/firebase_options.dart';
+import 'package:todo_frontend/splash_screen.dart';
 
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Homepage(),
+      home: SplashScreen(),
     );
   }
 }
